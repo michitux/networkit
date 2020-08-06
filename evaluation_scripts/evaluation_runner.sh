33eb32e994df00f93ac573b80e14043bc09af4f9
Luise <ufziw@student.kit.edu>
2020-08-06 08:55:12 +0200

    More improvements on evaluation script

--- evaluation_scripts/evaluation_runner.sh
+++ evaluation_scripts/evaluation_runner.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 set -ex
-graph_sets='small_graphs social_network web biological'
-scenarios='full plateauBound withoutBucketQueue'
+graph_sets='small_graphs social_network web'
+#scenarios='full plateauBound withoutBucketQueue'
+scenarios='full plateauBound'
 seeds='0 1 2 3 4 5 6 7 8 9'
 limit=400
 scenario=full
 
+declare -A graphs
+graphs=(["small_graphs"]='adjnoun.graph dolphins.graph karate.graph lesmis.graph football.graph terrorist.edgelist grass_web.pairs jazz.graph polbooks.graph' 
+["social_network"]='amazon.edgelist facebook100/Caltech36.mat dblp.edgelist youtube.edgelist facebook100/Penn94.mat lj.edgelist orkut.edgelist'
+["web"]='cnr-2000.graph in-2004.graph eu-2005.graph uk-2002.graph')
 
-small_graphs='adjnoun.graph dolphins.graph email.graph karate.graph lesmis.graph football.graph terrorist.edgelist grass_web.pairs'
-social_network='amazon.edgelist facebook100/Caltech36.mat dblp.edgelist youtube.edgelist facebook100/Penn94.mat lj.edgelist orkut.edgelist'
-web='cnr-2000.graph in-2004.graph eu-2005.graph uk-2002.graph'
+#small_graphs='adjnoun.graph dolphins.graph karate.graph lesmis.graph football.graph terrorist.edgelist grass_web.pairs jazz.graph polbooks.graph'
+#social_network='amazon.edgelist facebook100/Caltech36.mat dblp.edgelist youtube.edgelist facebook100/Penn94.mat lj.edgelist orkut.edgelist'
+#web='cnr-2000.graph in-2004.graph eu-2005.graph uk-2002.graph'
 
 #graph_set='facebook'
 #scenario='simple'
@@ -27,7 +32,33 @@ web='cnr-2000.graph in-2004.graph eu-2005.graph uk-2002.graph'
 
 
 
-python3 bionames.py -l ${limit}
+limit=400
+input="bio${limit}.txt"
+scenario='full'
+while IFS= read -r graph
+do
+  for seed in $seeds; do
+    python3 evaluation.py -g ${graph} -p "../output/QTM/biological${limit}/temp_${scenario}/" -s ${scenario} -r ${seed} &
+    printf "%s\t%s\t%s\t%d\n" ${graph_set} ${graph} ${scenario} ${seed}
+  done
+  wait
+done < "$input"
+python3 means.py -p "../output/QTM/biological${limit}/temp_${scenario}/"
+
+
+
+scenario='plateauBound'
+while IFS= read -r graph
+do
+  for seed in $seeds; do
+    python3 evaluation.py -g ${graph} -p "../output/QTM/biological${limit}/temp_${scenario}/" -s ${scenario} -r ${seed} &
+    printf "%s\t%s\t%s\t%d\n" ${graph_set} ${graph} ${scenario} ${seed}
+  done
+  wait
+done < "$input"
+python3 means.py -p "../output/QTM/biological${limit}/temp_${scenario}/"
+
+limit=0
 input="bio${limit}.txt"
 scenario='full'
 while IFS= read -r graph
@@ -41,66 +72,20 @@ python3 means.py -p "../output/QTM/biological${limit}/temp_${scenario}/"
 
 
 
-scenario='plateauBound'
-while IFS= read -r graph
-do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/biological${limit}/temp_${scenario}/" -s ${scenario} -r ${seed} &
+for graph_set in $graph_sets; do
+  for scenario in $scenarios; do
+    graph_list=${graphs[$graph_set]}
+    for graph in $graph_list; do
+      for seed in $seeds; do
+        python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r ${seed} &
+      done
+      wait
+    done
+    python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
   done
-  wait
-done < "$input"
-python3 means.py -p "../output/QTM/biological${limit}/temp_${scenario}/"
-
-
-scenario='withoutBucketQueue'
-while IFS= read -r graph
-do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/biological${limit}/temp_${scenario}/" -s ${scenario} -r ${seed} &
-  done
-  wait
-done < "$input"
-python3 means.py -p "../output/QTM/biological${limit}/temp_${scenario}/"
-#rm ${input}
-
-
-graph_set='social_network'
-scenario='full'
-for graph in $social_network1; do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r ${seed} &
-  done
-  wait
 done
-python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
 
-scenario='plateauBound'
-for graph in $social_network1; do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r ${seed} &
-  done
-  wait
-done
-python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
 
-graph_set='web'
-scenario='full'
-for graph in $social_network1; do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r ${seed} &
-  done
-  wait
-done
-python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
-
-scenario='plateauBound'
-for graph in $social_network1; do
-  for seed in $seeds; do
-    python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r ${seed} &
-  done
-  wait
-done
-python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
 
     
 
