44f0a0deb21fb777d2f873e404e69cab0823c6ca
Luise <ufziw@student.kit.edu>
2020-08-13 14:54:08 +0200

    Fix taskset bug

--- evaluation_scripts/evaluation_runner_big.sh
+++ evaluation_scripts/evaluation_runner_big.sh
@@ -13,16 +13,16 @@ for graph_set in $graph_sets; do
   for scenario in $scenarios; do
     graph_list=${graphs[$graph_set]}
     for graph in $graph_list; do
-      taskset 0 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 0 &
-      taskset 1 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 1 &
-      taskset 2 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 2 &
-      taskset 3 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 3 &
-      taskset 4 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 4 &
-      taskset 8 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 5 &
-      taskset 9 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 6 &
-      taskset 10 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 7 &
-      taskset 11 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 8 &
-      taskset 12 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 9 &
+      taskset -c 0 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 0 &
+      taskset -c 1 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 1 &
+      taskset -c 2 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 2 &
+      taskset -c 3 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 3 &
+      taskset -c 4 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 4 &
+      taskset -c 8 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 5 &
+      taskset -c 9 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 6 &
+      taskset -c 10 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 7 &
+      taskset -c 11 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 8 &
+      taskset -c 12 python3 evaluation.py -g ${graph} -p "../output/QTM/${graph_set}/temp_${scenario}/" -s ${scenario} -r 9 &
       wait
     done
     python3 means.py -p "../output/QTM/${graph_set}/temp_${scenario}/"
