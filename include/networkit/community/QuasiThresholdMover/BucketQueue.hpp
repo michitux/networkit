0deeb0181ceac86ed754d24857ec093d43067cda
Michael Hamann <michael.hamann@kit.edu>
2020-10-02 10:53:03 +0200

    QTM: BucketQueue: format files

--- include/networkit/community/QuasiThresholdMover/BucketQueue.hpp
+++ include/networkit/community/QuasiThresholdMover/BucketQueue.hpp
@@ -1,31 +1,30 @@
 #ifndef BUCKETQUEUE_H
 #define BUCKETQUEUE_H
 
-#include <networkit/graph/Graph.hpp>
 #include <networkit/community/QuasiThresholdMover/DynamicForest.hpp>
+#include <networkit/graph/Graph.hpp>
 
 namespace NetworKit {
-  
-  namespace QuasiThresholdMoving {
-    class BucketQueue {
-    public:
-      BucketQueue(count n = 0);  
-      void fill(const std::vector<node> &elements, const DynamicForest &dynamicForest);
-      void insertParent(node p);
-      node next();
-      bool empty() const;
-      std::string printQueue() const;
-      
-    private:
-      count nextNode;
-      count currentBucket;
-      count n;
-      std::vector<node> nodes;
-      //points to first element in the bucket
-      std::vector<count> border;
-    };
-  } //namespace QuasiThresholdMoving
 
+namespace QuasiThresholdMoving {
+class BucketQueue {
+public:
+    BucketQueue(count n = 0);
+    void fill(const std::vector<node> &elements, const DynamicForest &dynamicForest);
+    void insertParent(node p);
+    node next();
+    bool empty() const;
+    std::string printQueue() const;
+
+private:
+    count nextNode;
+    count currentBucket;
+    count n;
+    std::vector<node> nodes;
+    // points to first element in the bucket
+    std::vector<count> border;
+};
+} // namespace QuasiThresholdMoving
 
 } // namespace NetworKit
 
