39912af1b3d6dc91d44c8b3f4d371c181cf65f30
Michael Hamann <michael.hamann@kit.edu>
2021-06-11 12:44:17 +0200

    CommunityDetectionAlgorithm: add setGraph method
    This allows changing the underlying graph to support algorithms that
    want to take a fully initialized CommunityDetectionAlgorithm instance
    and execute it on different graphs.

--- networkit/cpp/community/CommunityDetectionAlgorithm.cpp
+++ networkit/cpp/community/CommunityDetectionAlgorithm.cpp
@@ -19,6 +19,12 @@ CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph& G) : Algor
 CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph &G, Partition baseClustering)
     : Algorithm(), G(&G), result(std::move(baseClustering)) {}
 
+void CommunityDetectionAlgorithm::setGraph(const Graph &G) {
+    this->G = &G;
+    hasRun = false;
+    result = Partition(0);
+}
+
 const Partition &CommunityDetectionAlgorithm::getPartition() const {
     if (!hasRun) {
         throw std::runtime_error("Call run()-function first.");
